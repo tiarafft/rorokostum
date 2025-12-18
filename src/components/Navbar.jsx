79173ba21cac89c84ddb82fc2f7c1b33fefe8fd4
tiarafft <arrifda4@gmail.com>
2025-12-18 15:08:03 +0700

    fixing logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
       <div className="container">
         <div className="navbar-content">
           <Link to="/" className="navbar-logo">
-            <img src="/image.png" alt="Roro Kostum Logo" className="logo-image" />
-            <span className="logo-text">RORO</span>
-            <span className="logo-subtitle">KOSTUM</span>
+            <img src="dist/logo.png" alt="Roro Kostum Logo" className="logo-image" />
+            <span className="logo-text">S</span>
+            <span className="logo-subtitle">Production</span>
           </Link>
 
           <button
